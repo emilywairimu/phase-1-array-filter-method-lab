f36f87c5f668b190cce8b8d649a61f181fd12c9f
Emily Wairimu <emily.wairimu@student.moringaschool.com>
2023-04-13 15:15:51 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution here
+const drivers = ['Carol', 'Mark', 'Alice', 'George', 'Patrick']
+
+const findMatching = (c,m) => c.filter(c => c.toLowerCase() === m.toLowerCase())
+
+const fuzzyMatch = (c,m) => c.filter(c => c.charAt(0) === m.charAt(0))
+
+const matchName = (c,m) => c.filter(c => c.name === m)
